af35a5d84490ffa2b576b58597e2f709eb752f84
VictorGiuliano <victorgiuliano98@gmail.com>
2023-01-29 23:12:10 +0100

    First part Milestone 3

--- src/components/utilities/WatchCard.vue
+++ src/components/utilities/WatchCard.vue
@@ -1,6 +1,12 @@
 <script>
+import { poster } from '../../data/index';
 export default {
     name: 'Card',
+    data() {
+        return {
+            poster
+        }
+    },
     props: {
         prod: Object
     },
@@ -12,7 +18,13 @@ export default {
         srcFlags() {
             const url = new URL(`../../assets/img/${this.prod.original_language}.png`, import.meta.url);
             return url.href;
-
+        },
+        posterUrl() {
+            let posterUrl = '';
+            if (this.prod.poster_path) {
+                posterUrl = poster + this.prod.poster_path;
+            }
+            return posterUrl;
         }
     }
 }
@@ -26,6 +38,7 @@ export default {
             <div v-else>{{ prod.original_language }}</div>
         </li>
         <li>{{ prod.vote_average }}</li>
+        <li><img :src="posterUrl" :alt="prod.original_name"></li>
     </ul>
 </template>
 <style>
--- src/data/index.js
+++ src/data/index.js
@@ -1,5 +1,6 @@
 const apiKey = "e6e673e2f8e14a5ac478f8be0f0b4361";
 const baseUri = "https://api.themoviedb.org/3";
 const language = "it-IT";
+const poster = "https://image.tmdb.org/t/p/w342";
 
-export { apiKey, baseUri, language }
+export { apiKey, baseUri, language, poster }
